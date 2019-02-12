b0c8fcf4e7fdc8d0e6a113d53923ffa45590004b
georgiy <mail@api.haus>
2019-02-12 15:07:53 +0300

    feat: parse appliation list

---
+++ src/common/parsing/parse-list.js
@@ -0,0 +1,70 @@
+import cheerio from "cheerio";
+
+export const NUMERIC_PRICE_REGEXP = /([\d]+[.,]?[\d]+)/;
+export const CURRENCY_REGEXP = /((?:(?!\.))[\D]+)/;
+
+/**
+ * @export
+ * @param {string} html
+ * @returns {import("google-play-scraping").ListApplication[]}
+ */
+export default function parseApplicationList(html) {
+	const $ = cheerio.load(html);
+
+	return $(".card")
+		.get()
+		.map(app => parseApp($(app)));
+}
+
+/**
+ * @export
+ * @param {Cheerio} $app
+ * @returns {import("google-play-scraping").ListApplication}
+ */
+export function parseApp($app) {
+	let formattedPrice = $app
+		.find("span.display-price")
+		.first()
+		.text();
+
+	// if price string contains numbers, it's not free
+	const isFree = !/\d/.test(formattedPrice);
+
+	let numericPrice = 0;
+	let currency = "";
+
+	if (!isFree) {
+		// @ts-ignore
+		const [, label = "0"] = formattedPrice.match(NUMERIC_PRICE_REGEXP) || [];
+		numericPrice = +label;
+		[, currency = ""] = formattedPrice.match(CURRENCY_REGEXP) || [];
+		currency = currency.trim();
+	}
+
+	const formattedScore = $app.find("div.tiny-star").attr("aria-label") || "";
+	let score = NaN;
+	if (formattedScore) {
+		score = parseFloat(formattedScore.match(/[\d.]+/)[0]);
+	}
+
+	return {
+		storeId: $app.attr("data-docid"),
+		title: $app.find("a.title").attr("title"),
+		summary: $app
+			.find("div.description")
+			.text()
+			.trim(),
+		developerName: $app.find("a.subtitle").text(),
+		developerId: $app
+			.find("a.subtitle")
+			.attr("href")
+			.split("id=")[1],
+		icon: $app.find("img.cover-image").attr("data-cover-large"),
+		score,
+		formattedScore,
+		formattedPrice,
+		currency,
+		numericPrice,
+		isFree,
+	};
+}
