6de5e5202bb21013f1e5fd45ccb46d5dd008825f
georgiy <mail@api.haus>
2019-02-12 15:15:08 +0300

    feat: export methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Category from "./category";
 import Collection from "./collection";
+import application from "application";
+import topCharts from "top-charts";
 
-export { Category, Collection };
+export { Category, Collection, application, topCharts };
