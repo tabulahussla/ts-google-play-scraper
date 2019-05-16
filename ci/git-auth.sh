7e71b9e643ed9f53a4d7836961f2439e8234cfca
Georgy Chirkov <mail@api.haus>
2019-05-16 19:35:00 +0300

    fix(ci): git auth

--- ci/git-auth.sh
+++ ci/git-auth.sh
@@ -5,8 +5,8 @@
 git remote set-url origin "https://gitlab.com/$CI_PROJECT_PATH.git"
 git config --global credential.helper store
 
-GIT_ASKPASS_USER=$(echo "$GIT_USERNAME" | base64 --decode)
-GIT_ASKPASS_PASSWORD=$(echo "$GIT_PASSWORD" | base64 --decode)
+GIT_ASKPASS_USER=$(echo "$GIT_USERNAME" | base64 -d)
+GIT_ASKPASS_PASSWORD=$(echo "$GIT_PASSWORD" | base64 -d)
 
 export GIT_ASKPASS_USER
 export GIT_ASKPASS_PASSWORD
