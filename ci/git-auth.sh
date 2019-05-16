67ec50903a8851a9d3e9cfb7ddca31268ca56d0f
Georgy Chirkov <mail@api.haus>
2019-05-16 18:09:51 +0300

    fix(ci): fix git auth

--- ci/git-auth.sh
+++ ci/git-auth.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
 
-apk --update add coreutils
+# apk --update add coreutils
 
 git remote set-url origin "https://gitlab.com/$CI_PROJECT_PATH.git"
 git config --global credential.helper store
 
-GIT_ASKPASS_USER=$(echo "$GIT_USERNAME" | base64 -d)
-GIT_ASKPASS_PASSWORD=$(echo "$GIT_PASSWORD" | base64 -d)
+GIT_ASKPASS_USER=$(echo "$GIT_USERNAME" | base64 --decode)
+GIT_ASKPASS_PASSWORD=$(echo "$GIT_PASSWORD" | base64 --decode)
 
 export GIT_ASKPASS_USER
 export GIT_ASKPASS_PASSWORD
-export GIT_ASKPASS="yarn git-askpass-env"
+export GIT_ASKPASS="./node_modules/.bin/git-askpass-env"
