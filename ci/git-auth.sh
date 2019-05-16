e2a35142d4b2ce78b1152a2a2644417b86fef056
Georgy Chirkov <mail@api.haus>
2019-05-16 18:01:14 +0300

    fix(ci): use local version of git-askpass-env

--- ci/git-auth.sh
+++ ci/git-auth.sh
@@ -5,11 +5,9 @@ apk --update add coreutils
 git remote set-url origin "https://gitlab.com/$CI_PROJECT_PATH.git"
 git config --global credential.helper store
 
-yarn global add git-askpass-env
-
 GIT_ASKPASS_USER=$(echo "$GIT_USERNAME" | base64 -d)
 GIT_ASKPASS_PASSWORD=$(echo "$GIT_PASSWORD" | base64 -d)
 
 export GIT_ASKPASS_USER
 export GIT_ASKPASS_PASSWORD
-export GIT_ASKPASS="git-askpass-env"
+export GIT_ASKPASS="yarn git-askpass-env"
