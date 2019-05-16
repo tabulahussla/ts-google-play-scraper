e99d1bfff99eef1265ddabc96ac86344ee5f5f86
Georgy Chirkov <mail@api.haus>
2019-05-16 20:31:57 +0300

    fix(lint): fix lint

--- src/publisher/parsing/index.ts
+++ src/publisher/parsing/index.ts
@@ -9,16 +9,16 @@ export default function parsePublisher(html) {
 	const $ = cheerio.load(html);
 
 	return {
-		name: $('[itemprop="name"]').text(),
-		description: $('[itemprop="description"]').text(),
-		icon: $('[srcset*="2x"]')
+		name: $("[itemprop=\"name\"]").text(),
+		description: $("[itemprop=\"description\"]").text(),
+		icon: $("[srcset*=\"2x\"]")
 			.slice(1)
 			.first()
 			.attr("src"),
-		headerImage: $('[srcset*="2x"]')
+		headerImage: $("[srcset*=\"2x\"]")
 			.first()
 			.attr("src"),
-		website: $('a:contains("Visit website")')
+		website: $("a:contains(\"Visit website\")")
 			.first()
 			.attr("href"),
 	};
