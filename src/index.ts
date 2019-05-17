a5b5d7f7335f0793849efe6526bb0ff99e166fb5
Georgy Chirkov <mail@api.haus>
2019-05-17 13:07:45 +0300

    fix(ts): generated typings module path

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import application from "~/application";
+import application from "~/application/index";
 import Category from "~/category";
 import Collection from "~/collection";
 import Pricing from "~/pricing";
-import publisher from "~/publisher";
-import search from "~/search";
-import topCharts from "~/top-charts";
+import publisher from "~/publisher/index";
+import search from "~/search/index";
+import topCharts from "~/top-charts/index";
 
 export {
 	Category,
